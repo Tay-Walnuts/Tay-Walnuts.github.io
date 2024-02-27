5412998bc6b104523c667152bbdb4e96ac1c0270
Tay-Walnuts <161483663+Tay-Walnuts@users.noreply.github.com>
2024-02-27 07:46:34 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+
+
+
+function Heading1(elementID){
+    const element = document.getElementById(elementID)
+
+    if (element && element.id === "H1") {
+        if (element.style.color === "aliceblue"){
+        element.style.color = "lightblue";
+        }
+        else if (element.style.color === "lightblue"){
+        element.style.color = "cornflowerblue";
+        }
+
+        else {
+        element.style.color = "aliceblue";
+        }
+    }
+}
+
+
+
+function HPonyo(elementID){
+    const element = document.getElementById(elementID)
+
+    if (element && element.id === "HeadingPONYO") {
+        if (element.style.color === "aliceblue"){
+        element.style.color = "burlywood";
+        }
+        else if (element.style.color === "burlywood"){
+        element.style.color = "coral";
+        }
+
+        else {
+        element.style.color = "aliceblue";
+        }
+    }
+}
+
+
+
+function SpiritAway(elementID){
+    const element = document.getElementById(elementID)
+
+    if (element && element.id === "SPIRAWAY") {
+        if (element.style.color === "aliceblue"){
+        element.style.color = "cadetblue";
+        }
+        else if (element.style.color === "cadetblue"){
+        element.style.color = "darkcyan";
+        }
+
+        else {
+        element.style.color = "aliceblue";
+        }
+    }
+}
+
+
+
+function NeighborToto(elementID){
+    const element = document.getElementById(elementID)
+
+    if (element && element.id === "MYNEIGHTOTO") {
+        if (element.style.color === "aliceblue"){
+        element.style.color = "lemonchiffon";
+        }
+        else if (element.style.color === "lemonchiffon"){
+        element.style.color = "goldenrod";
+        }
+
+        else {
+        element.style.color = "aliceblue";
+        }
+    }
+}
